478f558be0b62bdd2de5dac573cc74b16aa5892f
Leo0114 <leonel0614@gmail.com>
2023-08-14 20:37:22 -0600

    sisisi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://Leo0114.github.io",
   base: "/HomeSmart",
-  experimental: { assets: true, viewTransitions: true },
-  integrations: [tailwind()],
+  experimental: {
+    assets: true,
+    viewTransitions: true
+  },
+  integrations: [tailwind(), preact()]
 });
